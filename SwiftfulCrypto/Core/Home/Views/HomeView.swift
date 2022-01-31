5143438dba20088064b3ce9842349b8e9e36dcf9
MIKKILINENI PAVAN KUMAR <pavanmikkilineni690@gmail.com>
2022-01-31 08:45:35 +0530

    Added HomeView and header

--- SwiftfulCrypto/Core/Home/Views/HomeView.swift
+++ SwiftfulCrypto/Core/Home/Views/HomeView.swift
@@ -8,13 +8,59 @@
 import SwiftUI
 
 struct HomeView: View {
+    
+    @State private var showPortfolio:Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            //background layer
+            Color.theme.background
+                .ignoresSafeArea()
+            //content layer
+            VStack{
+                header
+                Spacer(minLength: 0)
+            }
+            .padding(.horizontal)
+            
+        }
+            
     }
 }
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
-        HomeView()
+        NavigationView{
+           HomeView()
+                .navigationBarHidden(true)
+        }
     }
+}
+
+extension HomeView{
+    
+    private var header:some View{
+        HStack{
+            CircleButtonView(iconName: showPortfolio ? "plus" : "info")
+                .animation(.none,value:showPortfolio)
+                .background(
+                    CircleButtonAnimationView(animate: $showPortfolio)
+                )
+            Spacer()
+            Text(showPortfolio ? "Protfolio" : "Live Prices")
+                .font(.headline)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.theme.accent)
+                .animation(.none,value:showPortfolio)
+            Spacer()
+            CircleButtonView(iconName: "chevron.right")
+                .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
+                .onTapGesture {
+                    withAnimation(.spring()){
+                        showPortfolio.toggle()
+                    }
+                }
+        }
+    }
+    
 }
--- SwiftfulCrypto/SwiftfulCryptoApp.swift
+++ SwiftfulCrypto/SwiftfulCryptoApp.swift
@@ -11,7 +11,10 @@ import SwiftUI
 struct SwiftfulCryptoApp: App {
     var body: some Scene {
         WindowGroup {
-           HomeView()
+            NavigationView{
+                HomeView()
+                    .navigationBarHidden(true)
+            }
         }
     }
 }
