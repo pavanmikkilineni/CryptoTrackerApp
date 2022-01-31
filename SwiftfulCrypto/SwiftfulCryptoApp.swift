3398e6ac13607ed65a055f12b958d80c8eaffc6e
MIKKILINENI PAVAN KUMAR <pavanmikkilineni690@gmail.com>
2022-01-31 07:13:09 +0530

    Initial Commit

---
+++ SwiftfulCrypto/SwiftfulCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulCryptoApp.swift
+//  SwiftfulCrypto
+//
+//  Created by Pavan Mikkilineni on 31/01/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
